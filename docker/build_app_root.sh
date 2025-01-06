25b4712407d8aad559196edea7e5e0ce061ab450
Marc Culler <culler@users.noreply.github.com>
2025-01-06 14:43:33 -0600

    Add docker build support.

---
+++ docker/build_app_root.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+docker run -d -t --name app_root_builder appimage_root/2014:v1 bash
+docker exec -w /tmp app_root_builder git clone https://github.com/3-manifolds/py_appimage
+docker exec -w /tmp app_root_builder make -C py_appimage
+#docker exec -w /tmp app_root_builder make -C py_appimage DockerFix
+docker exec -w /tmp app_root_builder make -C py_appimage Tarball
+docker cp app_root_builder:/tmp/py_appimage/app_root-3.13.tgz .
+docker cp app_root_builder:/tmp/py_appimage/app_root-3.13.sha256 .
+docker stop app_root_builder
+docker container rm app_root_builder
---
+++ docker/build_docker_image.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+docker build -t appimage_root/2014:v1 .
