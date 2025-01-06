41af0a7f27ca9922861d3d7b54bade915ea54325
Marc Culler <culler@users.noreply.github.com>
2025-01-06 17:50:19 -0600

    Use the tcl-core archive so we don't have to build sqlite3 twice.

--- TclTk/fetch_tcltk.sh
+++ TclTk/fetch_tcltk.sh
@@ -4,10 +4,13 @@ set -e
 TCL_VERSION=9.0.1
 TK_VERSION=9.0.1
 
+TCL_TARBALL=tcl-core${TCL_VERSION}-src.tar.gz
+TK_TARBALL=tk${TK_VERSION}-src.tar.gz
+
 mkdir Tcl Tk
 
-curl -L -O https://prdownloads.sourceforge.net/tcl/tcl$TCL_VERSION-src.tar.gz
-curl -L -O https://prdownloads.sourceforge.net/tcl/tk$TK_VERSION-src.tar.gz
+curl -L -O https://prdownloads.sourceforge.net/tcl/${TCL_TARBALL}
+curl -L -O https://prdownloads.sourceforge.net/tcl/${TK_TARBALL}
 
-tar xf tcl$TCL_VERSION-src.tar.gz --directory=Tcl --strip-components=1
-tar xf tk$TK_VERSION-src.tar.gz --directory=Tk --strip-components=1
+tar xf ${TCL_TARBALL} --directory=Tcl --strip-components=1
+tar xf ${TK_TARBALL} --directory=Tk --strip-components=1
